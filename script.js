e0b0a2e2ff78928d3f941e25929d2b99bd8f258b
Jacob Templeton <jwctempleton@gmail.com>
2022-03-04 15:42:39 -0800

    Add functions to create grid elements and append them to grid container, build out default grid on page load

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 // Query Selectors
 const gridContainer = document.querySelector(".grid");
+
+//Event Listeners
+window.addEventListener("load", setDefaultGrid);
+
+// Functions
+function setGridSize(size) {
+  gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+}
+
+function fillGrid(size) {
+  for (let i = 0; i < size * size; i++) {
+    const gridElement = document.createElement("div");
+    gridElement.classList = "grid-element";
+    gridContainer.appendChild(gridElement);
+  }
+}
+
+function setDefaultGrid() {
+  setGridSize(16);
+  fillGrid(16);
+}
